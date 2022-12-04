ee4ddc5fecbc072a9324e22055a935ba32eb8642
awaisrehmat <100486479+awaisrehmat@users.noreply.github.com>
2022-12-04 14:11:22 +0500

    .

--- app/App.js
+++ app/App.js
@@ -15,17 +15,23 @@ export default function App() {
     setnumber(number+5)
     setcounter(counter+1)
     setCurrent(false)
-
+ 
   }
 
   return (
 
     <View style={styles.container}>
+      <View style={{flex:2,margin:10,backgroundColor:'yellow',height:'95%',width:'95%',borderColor:'black',borderRadius:20}}>
+        <View style={{margin:50}}>
       <Text>{number}</Text>
       <Text>{Current ? 'Before' : 'After'}</Text>
       <Button title='push me' onPress={Press}></Button>
+      <Button title='Reset' onPress= {()=>{setcounter(0),setnumber(0)}}></Button>
       <Text>Button tapped {counter} times</Text>
       <StatusBar style="auto" />
+      
+        </View>
+      </View>
     </View>
   );
 }
