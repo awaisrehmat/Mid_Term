18fcc20d498a650b4ffc1361e3071e41eef405dc
awaisrehmat <100486479+awaisrehmat@users.noreply.github.com>
2022-12-10 22:18:02 +0500

    .

--- app/App.js
+++ app/App.js
@@ -2,17 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react'; 
 import { useState } from 'react';
 import { StyleSheet, Text, View,Button, ScrollView, Image } from 'react-native';
+import SignIn from './Components/SignIn'
 
 export default function App() {
-
-
   return (
     <View>
-
-       <Image 
-       style = {styles.logo}
-       source = {require("./assets/logo.png")}/>
-       <text>hi</text>
+        <SignIn />
     </View>
 
 
@@ -23,19 +18,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:50,
-    //flexDirection:'row',
-    // backgroundColor: 'blue',
-    // alignItems: 'center',
-    // justifyContent: 'space-around',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  logo:{
-height: 50,
-width: 40
-
+  
 
   }
  
   
 
-});
+);
